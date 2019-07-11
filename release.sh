f9ba9e2cb9bd0316ecf77d52539168e731dcbf51
Philipp Hug <philipp@hug.cx>
2019-07-11 16:13:24 +0200

    use quay.io to build, adapt documentation

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-TAG="v`date +%Y%m%d`-`git describe --match=NeVeRmAtCh --always --dirty`"
-IMAGE=abasky-auth-addons
-REGISTRY=gcr.io/abasky-images
-
-docker build . -t $REGISTRY/$IMAGE:$TAG
-docker push $REGISTRY/$IMAGE:$TAG
